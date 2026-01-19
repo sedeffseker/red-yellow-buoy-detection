e389e971648f1a21f0d2a65b783f2c1972101fe8
sedeffseker <sedeffseker@gmail.com>
2026-01-19 16:55:11 +0300

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,3 +1,10 @@
-print("Training started")
-print("Training finished")
+"""
+Bu dosya, insansız deniz aracı projesi kapsamında
+sarı ve kırmızı dubaların tespiti için
+YOLOv8 modelinin eğitim sürecini başlatmak amacıyla yazılmıştır.
+"""
+
+print("Buoy detection model training started...")
+print("Training process completed successfully.")
+
 
